6c386e31b8e8f2d5f78e3faeac6fa1b6b473effd
Jason.z <281674669@qq.com>
2025-08-30 15:04:11 +0800

    修改忽略

--- unpackage/dist/build/.nvue/app.js
+++
@@ -1,8 +0,0 @@
-Promise.resolve("./pages/index/queue.js").then((res) => {
-  res();
-});
-Promise.resolve("./pages/index/test.js").then((res) => {
-  res();
-});
-Promise.resolve("./app.css.js").then(() => {
-});
